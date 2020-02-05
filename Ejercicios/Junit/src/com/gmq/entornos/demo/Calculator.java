d90dfea7f6cd158a701327186ac71017df5e5d7c
Raul Camarero <raul.camarero@fon.com>
2020-02-05 19:02:59 +0100

    Calculator Junit exercices

---
+++ Ejercicios/Junit/src/com/gmq/entornos/demo/Calculator.java
@@ -0,0 +1,18 @@
+package com.gmq.entornos.demo;
+
+import java.math.BigDecimal;
+
+public interface Calculator {
+
+    BigDecimal sum(BigDecimal firstSummand, BigDecimal secondSummand);
+
+    BigDecimal substract(BigDecimal minuend, BigDecimal subtrahend);
+
+    BigDecimal multiply(BigDecimal multiplicand, BigDecimal multiplier);
+
+    BigDecimal divide(BigDecimal dividend, BigDecimal divisor);
+
+    BigDecimal mcd(BigDecimal first, BigDecimal second);
+
+    BigDecimal mcm(BigDecimal first, BigDecimal second);
+}
---
+++ Ejercicios/Junit/src/com/gmq/entornos/demo/EnvironmentCalculator.java
@@ -0,0 +1,36 @@
+package com.gmq.entornos.demo;
+
+import java.math.BigDecimal;
+
+public class EnvironmentCalculator implements Calculator {
+
+    @Override
+    public BigDecimal sum(BigDecimal firstSummand, BigDecimal secondSummand) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal substract(BigDecimal minuend, BigDecimal subtrahend) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal multiply(BigDecimal multiplicand, BigDecimal multiplier) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal divide(BigDecimal dividend, BigDecimal divisor) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal mcd(BigDecimal first, BigDecimal second) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal mcm(BigDecimal first, BigDecimal second) {
+        return null;
+    }
+}
