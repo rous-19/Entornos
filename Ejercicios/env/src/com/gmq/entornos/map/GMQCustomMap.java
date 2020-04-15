62017823a2ba02908bec470871285cc8db6fc076
Raul Camarero <raul.camarero@fon.com>
2020-04-15 21:28:13 +0200

    Solution

--- Ejercicios/env/src/com/gmq/entornos/map/GMQCustomMap.java
+++ Ejercicios/env/src/com/gmq/entornos/map/GMQCustomMap.java
@@ -3,10 +3,23 @@ package com.gmq.entornos.map;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * GMQCustom implementation of a map
+ * 
+ * @author Raul.Camarero
+ *
+ */
 public class GMQCustomMap implements CustomMap<Integer, String> {
 
 	private Map<Integer, String> embebbedMap = new HashMap<Integer, String>();
 	
+	/**
+	 * Adds a key and a value to the CustomMap
+	 * 
+	 * @param key unique key in map
+	 * @param value value for a key
+	 * @return current CustomMap
+	 */
 	@Override
 	public CustomMap<Integer, String> put(Integer key, String value) {
 
@@ -14,6 +27,12 @@ public class GMQCustomMap implements CustomMap<Integer, String> {
 		return this;
 	}
 
+	/**
+	 * Checks if the key is present
+	 * 
+	 * @param key present
+	 * @return true if present
+	 */
 	@Override
 	public boolean containsKey(Integer key) {
 		
@@ -21,6 +40,13 @@ public class GMQCustomMap implements CustomMap<Integer, String> {
 		return embebbedMap.containsKey(key);
 	}
 
+	/**
+	 * 
+	 * Returns value from key in map
+	 * 
+	 * @param key the key
+	 * @return String the value
+	 */
 	@Override
 	public String get(Integer key) {
 
